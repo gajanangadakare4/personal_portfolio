7cfc020e65e54265ebb4ddea30c90961d28fbe5f
Gajanan Gadakare <128011340+gajanangadakare4@users.noreply.github.com>
2025-12-27 08:01:37 +0530

    Increase max width of About Me
    Updated paragraph width for better readability.

--- components/About.tsx
+++ components/About.tsx
@@ -7,7 +7,7 @@ export default function About() {
         About Me
       </h2>
 
-      <p className="text-zinc-400 max-w-3xl mx-auto text-lg leading-relaxed text-justify">
+      <p className="text-zinc-400 max-w-5xl mx-auto text-lg leading-relaxed text-justify">
         I am a dedicated Full-Stack Developer with hands-on experience across all phases of the Software Development Life Cycle (SDLC), including requirements analysis, system design, development, testing, deployment, and maintenance. I build secure, scalable, and high-performance applications by applying software engineering principles, clean architecture, and industry best practices, while continuously enhancing my expertise in cybersecurity, machine learning, and ethical hacking.
       </p>
     </section>
